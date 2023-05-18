c061f0f317cdf7765c1b547c87bee05cc6c733f1
Joismar Braga <joismar@outlook.com>
2023-05-17 22:06:29 -0300

    feat: create auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { LoginDto } from 'src/auth/dtos/login.dto';
+import { SignUpDto } from './dtos/signup.dto';
+
+@Controller('auth')
+@ApiTags('Auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() loginDto: LoginDto) {
+    return this.authService.signIn(loginDto.email, loginDto.password);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signup')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { hashSync, compareSync, genSaltSync } from 'bcrypt';
+import { SignUpDto } from './dtos/signup.dto';
+import { UserDto } from 'src/users/dtos/user.dto';
+import { IUserRepository, USER_REPOSITORY } from 'src/shared/interfaces/user-repository.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(USER_REPOSITORY)
+    private readonly usersRepository: IUserRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto): Promise<void> {
+    const encryptedPassword = this.encryptPassword(signUpDto.password);
+    const newUser: UserDto = {
+      ...signUpDto,
+      password: encryptedPassword,
+    };
+
+    await this.usersRepository.create(newUser);
+  }
+
+  async signIn(email: string, pass: string): Promise<any> {
+    const user = await this.usersRepository.findOne({ email })
+    if (!user || !this.comparePasswords(pass, user.password)) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+
+    const token = this.generateToken(user);
+
+    return { ...result, token };
+  }
+
+  private encryptPassword(password: string): string {
+    const saltRounds = 10;
+    const salt = genSaltSync(saltRounds);
+    return hashSync(password, salt);
+  }
+
+  private comparePasswords(plainPassword: string, encryptedPassword: string): boolean {
+    return compareSync(plainPassword, encryptedPassword);
+  }
+
+  private generateToken(user: any): string {
+    const payload = { email: user.email, sub: user.id };
+    return this.jwtService.sign(payload);
+  }
+}
